06264278e1e572a336f1465cb111b84726abef8d
Cristi <cristianbulai89@gmail.com>
2019-11-20 20:47:30 +0200

    new insert

---
+++ src/Heroes/Hero.java
@@ -0,0 +1,27 @@
+package Heroes;
+
+public abstract class Hero {
+    protected String name;
+    protected int hp;
+
+    public Hero(String name, int hp, int mana) {
+        this.name = name;
+        this.hp = hp;
+        this.mana = mana;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", hp=" + hp +
+                ", mana=" + mana +
+                '}';
+    }
+
+    protected int mana;
+
+    public abstract void firePrimary();
+    public abstract void fireSecondary();
+    public abstract void receiveHit();
+}
