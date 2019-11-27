0f373c2134f774f527fbbfaf73457e1416558686
Cristi <cristianbulai89@gmail.com>
2019-11-27 20:41:59 +0200

    OK

--- src/Songs/OrderArtist.java
+++ src/Songs/OrderArtist.java
@@ -8,20 +8,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.TreeSet;
 
-public class OrderSongs {
+public class OrderArtist {
     public static void main(String[] args) {
         try {
-            BufferedReader file = new BufferedReader(new FileReader("songs.txt"));
+            BufferedReader file = new BufferedReader(new FileReader("titles.txt"));
             String line;
-            TreeSet<String> songs = new TreeSet<>();
+            TreeSet<Song> songs = new TreeSet<>();
             while ((line = file.readLine()) != null) {
-                songs.add(line);
+                String[] parts = line.split("/");
+                Song song = new Song(parts[0], parts[1]);
+                songs.add(song);
             }
             System.out.println(songs.size());
             System.out.println(songs);
 
-//            Collections.shuffle(songs);
-//            System.out.println(songs);
+//            Collections.sort(songs, new ArtistComparator());
+            System.out.println(songs);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -30,3 +32,4 @@ public class OrderSongs {
         }
     }
 }
+
